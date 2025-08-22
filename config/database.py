1ffcdc0d8a5dfc98409ff9a5122bc925e465b6ab
Carlos Roberto <carlosjr.if@gmail.com>
2025-08-21 22:50:12 -0300

    feat: add initial config

---
+++ config/database.py
@@ -0,0 +1,31 @@
+from typing import Generator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+import os
+
+DATABASE = {
+    "drivername": os.getenv("DATABASE_DRIVERNAME", "mysql+pymysql"),
+    "host": os.getenv("DATABASE_HOST", "localhost"),
+    "port": os.getenv("DATABASE_PORT", "3307"),
+    "user": os.getenv("DATABASE_USER", "user"),
+    "password": os.getenv("DATABASE_PASSWORD", "password"),
+    "name": os.getenv("DATABASE_DATABASE", "db_name"),
+}
+
+DATABASE_URL = (
+    f"{DATABASE['drivername']}://{DATABASE['user']}:{DATABASE['password']}@"
+    f"{DATABASE['host']}:{DATABASE['port']}/{DATABASE['name']}"
+)
+
+DELETE_MODE = os.getenv("DELETE_MODE", "soft")
+
+engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True, pool_recycle=3600)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
