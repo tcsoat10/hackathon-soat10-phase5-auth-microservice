890846ccdada27471ea1af71aafeb66a25f27b9c
Carlos Roberto <carlosjr.if@gmail.com>
2025-08-26 23:32:30 -0300

    chore(code-removal): remove HealthCheckUseCase

--- src/application/usecases/health_check_usecase/health_check_usecase.py
+++
@@ -1,8 +0,0 @@
-from src.schemas.health_check_schema import HealthCheckSchemaOut
-from src.core.domain.entities.health_check import HealthCheck
-
-
-class HealthCheckUseCase:
-    def execute(self) -> HealthCheck:
-        health_check = HealthCheck(status="healthy")
-        return HealthCheckSchemaOut.from_entity(health_check)
